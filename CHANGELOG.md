a3f5332c74b53e7a75be4f3bdf6834da49e2635b
Unknown <jafjdev@gmail.com>
2019-03-04 01:55:28 +0100

    Solo interfaz hasta el perfil de usuario

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+# 17 December 2018 - v1.1.0
+- Update to Angular 7
+- Removed webpack process and replaced it with tsc
+- Updated all libraries to its latest versions
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  BASE_URL: 'http://localhost:8000'
+  // when you deploy your app to your own server you should replace the BASE_URL with the target domain for example:
+  // BASE_URL: 'https://site-preview.angular-templates.io'
+};
