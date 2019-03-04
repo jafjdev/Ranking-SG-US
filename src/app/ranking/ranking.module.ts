1dcd26ee27fcc2db52542ba5214c2006739a6f38
Manuel Espinoza <manuel.espinoza1996@gmail.com>
2019-03-04 09:14:33 -0400

    Tabla fea

--- src/app/ranking/ranking.module.ts
+++ src/app/ranking/ranking.module.ts
@@ -3,6 +3,10 @@ import { CommonModule } from '@angular/common';
 import { AuthService } from '../auth/services/auth.service';
 import { RouterModule } from '@angular/router';
 import { RankingComponent } from './ranking.component';
+import { SharedModule } from '../shared';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 
 export const rankingRoutes = [
   {
@@ -19,6 +23,10 @@ export const rankingRoutes = [
   imports: [
     RouterModule.forChild(rankingRoutes),
     CommonModule,
+    SharedModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatButtonToggleModule
   ],
   providers: []
 })
